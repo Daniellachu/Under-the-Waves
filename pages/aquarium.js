60aff3a21a460a61ff035caf33930038d70991d5
daniellachu <delacruzdanielleb@gmail.com>
2023-04-06 12:35:41 -0700

    linking css to pages and add function names

--- pages/aquarium.js
+++ pages/aquarium.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/aquarium.module.css'
 
-export default function Home() {
+export default function Aquarium() {
     return (
       <>
         <Head>
