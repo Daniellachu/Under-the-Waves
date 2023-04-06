acc315a6cd6cb3ca407e6debd62728e8a2cbb816
Yanjun Liu <yanjunlbcit@gmail.com>
2023-04-06 11:04:18 -0700

    adding the navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,5 +1,6 @@
 import styles from "./navbar.module.css"
 import Image from 'next/image'
+import Link from "next/link"
 
 export default function Navbar() {
 
@@ -7,24 +8,30 @@ export default function Navbar() {
         <>
             <div className={styles.main}>
                 <div className={styles.icons}>
-                    <Image
-                        className={styles.homeicon}
-                        src={"/homeicon.png"}
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        className={styles.shopicon}
-                        src={"/shopicon.png"}
-                        width={50}
-                        height={50}
-                    />
-                    <Image
-                        className={styles.beachicon}
-                        src={"/beachicon.png"}
-                        width={50}
-                        height={50}
-                    />
+                    <Link href={"/home"}>
+                        <Image
+                            className={styles.shopicon}
+                            src={"/home.png"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                    <Link href={"/beach"}>
+                        <Image
+                            className={styles.beachicon}
+                            src={"/beach.png"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                    <Link href={"/shop"}>
+                        <Image
+                            className={styles.shopicon}
+                            src={"/shop.png"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
                 </div>
                 <div className={styles.base}>
 
