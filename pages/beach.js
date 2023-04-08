a19e2290abb13d2de63ca058a1bf8072faf60d77
Daniellachu <delacruzdanielleb@gmail.com>
2023-04-07 20:22:14 -0700

    beach background test 2

--- pages/beach.js
+++ pages/beach.js
@@ -14,7 +14,7 @@ export default function Beach() {
       </Head>
       <main className={styles.main}>
         <h1>Beach</h1>
-        <img src='Pixel/Background/under the waves background.svg'></img>
+        <img src='Pixel/Background/testbg4.svg'></img>
         <Navbar></Navbar>
       </main>
     </>
