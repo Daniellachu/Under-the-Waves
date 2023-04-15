dca5b67de11c96a49fdd8c8096fd1631e1b0c50c
TeoEstrada9 <mateo.feriaestrada@gmail.com>
2023-04-15 12:11:06 -0700

    wip topnavbar

--- components/top-navbar/index.js
+++ components/top-navbar/index.js
@@ -1,85 +1,114 @@
-import Image from 'next/image'
-import Link from "next/link"
-import Topnavbar from '@/components/top-navbar'
-import styles from '@/components/top-navbar/top-navbar.module.css'
-import { useState } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import Topnavbar from "@/components/top-navbar";
+import styles from "@/components/top-navbar/top-navbar.module.css";
+import { useState } from "react";
 
-export default function topNavbar() {
+export default function topNavbar({ title = "" }) {
+  const screens = {
+    main: "main",
+    save: "save",
+    saveConfirm: "saveConfirm",
+    settings: "settings",
+    quit: "quit",
+  };
+  const [screen, setScreen] = useState("");
+  // screen switching based on switch statement
 
-    const [popup, setPopup] = useState(true);
+  // overall layout
 
-    return (
-        <>
-            <div className={styles.main}>
-                <div className={styles.icons_part}>
-                    <Link href={"/quiz"}>
+  // top bar navigation itself
+  // quiz and hamburger menu icon
+
+  const renderScreen = () => {
+    switch (screen) {
+      case screens.main:
+        return (
+          <div className={`${styles.popupMenu} ${styles.mainScreen}`}>
+            <Image
+              className={styles.popupMenuBox}
+              src={"/boxes/menu_box.png"}
+              width={300}
+              height={500}
+            />{" "}
+            <button onClick={() => setScreen("")} className={styles.btn}>
+              <Image
+                src={"./buttons/continue.svg"}
+                alt={"./buttons/continue.svg"}
+                width={155}
+                height={50}
+              />
+            </button>
+            <button onClick={() => setScreen(screens.save)} className={styles.btn}>
+              <Image
+                src={"./buttons/save.svg"}
+                alt={"./buttons/save.svg"}
+                width={155}
+                height={50}
+              />
+            </button>
+            <button className={styles.btn}>
+              <Image
+                src={"./buttons/settings.svg"}
+                alt={"./buttons/settings.svg"}
+                width={155}
+                height={50}
+              />
+            </button>
+            <Link href={"/"} className={styles.btn}>
+              <Image
+                src={"./buttons/quit.svg"}
+                alt={"./buttons/quit.svg"}
+                width={155}
+                height={50}
+              />
+            </Link>
+          </div>
+        );
+        case screens.save:
+            return (
+                <div className={`${styles.popupMenuWide}`}>
+                    <Image
+                    src={"/boxes/wide_box.png"} 
+                    alt={"/boxes/wide_box.png"}
+                    width={90}
+                    height={90}
+                    className={styles.popupMenuBox}
+                    />
+                    <div className={styles.popupMenuContent}>
+                        <button className={`${styles.btn} ${styles.closeBtn}`}>X</button>
+                        <p>You have unsaved progress, please save your progress</p>
+                        <button className={styles.btn}>
                         <Image
-                            className={styles.menu_icon}
-                            src={"./icons/quiz.svg"}
-                            width={50}
+                            src={"./buttons/save.svg"}
+                            alt={"./buttons/save.svg"}
+                            width={155}
                             height={50}
                         />
-                    </Link>
-                    {
-                        popup ?
-                            <div className={styles.icon}>
-                                <div>
-                                    <div onClick={() => setPopup(false)}>
-                                        <Image
-                                            className={styles.menu_icon}
-                                            src={"./icons/menu.svg"}
-                                            width={50}
-                                            height={50}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                            : 
-                            <div className={styles.popup}>
-                                <Image 
-                                    className={styles.background_image}
-                                    src={"/boxes/menu_box.png"}
-                                    width={300}
-                                    height={500}
-                                />
-                                <div className={styles.menu_buttons}>
-                                    <p onClick={() => setPopup(true)}>
-                                        <Image
-                                            className={styles.buttons}
-                                            src={"./buttons/continue.svg"}
-                                            width={155}
-                                            height={50}
-                                        />
-                                    </p>
-                                    <Link href={"/save_1"}>
-                                        <Image
-                                            className={styles.buttons}
-                                            src={"./buttons/save.svg"}
-                                            width={155}
-                                            height={50}
-                                        />
-                                    </Link>
-                                    <Link href={"/settings"}>
-                                        <Image
-                                            className={styles.buttons}
-                                            src={"./buttons/settings.svg"}
-                                            width={155}
-                                            height={50}
-                                        />
-                                    </Link>
-                                    <Link href={"/"}>
-                                        <Image
-                                            className={styles.buttons}
-                                            src={"./buttons/quit.svg"}
-                                            width={155}
-                                            height={50}
-                                        />
-                                    </Link>
-                                </div>
-                            </div>
-                    }
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            )
+      default:
+        return;
+    }
+  };
+
+  return (
+    <>
+      <nav className={styles.topNavbar}>
+        <Link href={"/quiz"} className={`${styles.quizBtn} ${styles.btn}`}>
+          <Image src={"./icons/quiz.svg"} width={50} height={50} />
+        </Link>
+        {title && <h1 className={styles.title}>{title}</h1>}
+        <button
+          onClick={() => setScreen(screens.main)}
+          className={`${styles.menuBtn} ${styles.btn}`}
+        >
+          <Image src={"./icons/menu.svg"} width={50} height={50} />
+        </button>
+      </nav>
+      {screen && <div className={styles.backDrop}>{renderScreen()}</div>}
+    </>
+  );
 }
