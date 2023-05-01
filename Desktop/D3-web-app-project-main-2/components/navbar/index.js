7e4a6f0a7048878bc2f3cfa74c06196d3f414ad0
JacobSkilling-dot <113203264+JacobSkilling-dot@users.noreply.github.com>
2023-05-01 09:16:19 -0700

    3rd quiz page

---
+++ Desktop/D3-web-app-project-main-2/components/navbar/index.js
@@ -0,0 +1,50 @@
+import styles from "./navbar.module.css"
+import Image from 'next/image'
+import Link from "next/link"
+
+export default function Navbar() {
+
+    return (
+        <>
+            <div className={styles.main}>
+                <div className={styles.icons}>
+                    <Link href={"/"}>
+                        <Image
+                            className={styles.icon}
+                            src={"./icons/home.svg"}
+                            width={55}
+                            height={55}
+                        />
+                    </Link>
+                    <Link href={"/beach"}>
+                        <Image
+                            className={styles.icon}
+                            src={"./icons/beach.svg"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                    <Link href={"/aquarium"}>
+                        <Image
+                            className={styles.icon}
+                            src={"./icons/aquarium.svg"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                    <Link href={"/market"}>
+                        <Image
+                            className={styles.icon}
+                            src={"./icons/market.svg"}
+                            width={50}
+                            height={50}
+                        />
+                    </Link>
+                </div>
+                <div className={styles.base}>
+
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ Desktop/D3-web-app-project-main-2/components/top-navbar/index.js
@@ -0,0 +1,148 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/components/top-navbar/top-navbar.module.css";
+import { useState } from "react";
+
+export default function topNavbar({ title = "" }) {
+
+  // gives us autocomplete instead of working with just strings
+  const screens = {
+    main: "main",
+    save: "save",
+    saveConfirm: "saveConfirm",
+    settings: "settings",
+    quit: "quit",
+  };
+  // this state determines whether or not to display a popup, and if so which one
+  const [screen, setScreen] = useState("");
+  // screen switching based on switch statement
+
+  // overall layout
+
+  // top bar navigation itself
+  // quiz and hamburger menu icon
+
+  const renderScreen = () => {
+    switch (screen) {
+      case screens.main: // "main"
+        return (
+          <div className={`${styles.popupMenu} ${styles.mainScreen}`}>
+            <Image
+              className={styles.popupMenuBox}
+              src={"/boxes/menu_box.png"}
+              width={300}
+              height={500}
+            />{" "}
+            <button onClick={() => setScreen("")} className={styles.btn}>
+              <Image
+                src={"./buttons/continue.svg"}
+                alt={"./buttons/continue.svg"}
+                width={155}
+                height={50}
+              />
+            </button>
+            <button
+              onClick={() => setScreen(screens.save)}
+              className={styles.btn}
+            >
+              <Image
+                src={"./buttons/save.svg"}
+                alt={"./buttons/save.svg"}
+                width={155}
+                height={50}
+              />
+            </button>
+            <Link href={"/settings"} className={styles.btn}>
+              <Image
+                src={"./buttons/settings.svg"}
+                alt={"./buttons/settings.svg"}
+                width={155}
+                height={50}
+              />
+            </Link>
+            <Link href={"/"} className={styles.btn}>
+              <Image
+                src={"./buttons/quit.svg"}
+                alt={"./buttons/quit.svg"}
+                width={155}
+                height={50}
+              />
+            </Link>
+          </div>
+        );
+      case screens.save: // "save"
+        return (
+          <div className={`${styles.popupMenuWide}`}>
+            <Image
+              src={"/boxes/wide_box.png"}
+              alt={"/boxes/wide_box.png"}
+              width={90}
+              height={90}
+              className={styles.popupMenuBox}
+            />
+            <div className={styles.popupMenuContent}>
+              <button onClick={() => setScreen("")}  className={`${styles.btn} ${styles.closeBtn}`}>X</button>
+              <p>You have unsaved progress, please save your progress</p>
+              <button onClick={() => setScreen(screens.saveConfirm) } className={styles.btn}>
+                <Image
+                  src={"./buttons/saveButton.svg"}
+                  alt={"./buttons/save.svg"}
+                  width={155}
+                  height={50}
+                />
+              </button>
+            </div>
+          </div>
+        );
+      case screens.saveConfirm: // "saveConfirm"
+        return (
+          <div className={`${styles.popupMenuWide}`}>
+            <Image
+              src={"/boxes/wide_box.png"}
+              alt={"/boxes/wide_box.png"}
+              width={90}
+              height={90}
+              className={styles.popupMenuBox}
+            />
+            <div className={styles.popupMenuContent}>
+              <button onClick={() => setScreen("")} className={`${styles.btn} ${styles.closeBtn}`}>X</button>
+              <p>Your Progress has been Saved</p>
+              <button className={styles.btn}>
+                <Image 
+                  src={"./buttons/back2game.svg"}
+                  alt={"./buttons/back2game.svg"}
+                  width={200}
+                  height={75}
+                />
+              </button>
+            </div>
+          </div>
+        );
+      default: // anything other than the values we declared above
+      // we either have a invalid value for screen, or a typo, etc.
+      // we reset screen to "" which prevents it from being rendered
+        setScreen("")
+      // break statement stops the switch statement
+        break;
+    }
+  };
+
+  return (
+    <>
+      <nav className={styles.topNavbar}>
+        <Link href={"/quiz"} className={`${styles.quizBtn} ${styles.btn}`}>
+          <Image src={"./icons/quiz.svg"} width={50} height={50} />
+        </Link>
+        {title && <h1 className={styles.title}>{title}</h1>}
+        <button
+          onClick={() => setScreen(screens.main)}
+          className={`${styles.menuBtn} ${styles.btn}`}
+        >
+          <Image src={"./icons/menu.svg"} width={50} height={50} />
+        </button>
+      </nav>
+      {/* if screen is an empty string ("") then nothing is rendered. If it is something other than an empty string, we render this JSX and call renderScreen() */}
+      {screen && <div className={styles.backDrop}>{renderScreen()}</div>}
+    </>
+  );
+}
